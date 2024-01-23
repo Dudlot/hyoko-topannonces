2fe5c10c519e7e399f6eeb8299e499e68d8c45c8
Charlotte Dunand <contact@charlotte-dunand.com>
2024-01-23 11:20:34 +0100

    Authentification et sécurisation de l’API - 23 jan. 2024

---
+++ server/src/models/user.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        firstname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastname: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: {msg: 'L\'adresse-mail est déjà prise.'},
+            validate: {
+                isEmail: {msg: 'L\'adresse-mail est invalide.'}
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    })
+}
