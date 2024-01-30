7a0c57a7aff270080725a5842ad9f8a6c548f99e
Charlotte Dunand <contact@charlotte-dunand.com>
2024-01-30 10:48:38 +0100

    Débogage Heroku #7

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -2,26 +2,43 @@ const {Sequelize, DataTypes} = require('sequelize')
 const AddModel = require('../models/add')
 const UserModel = require('../models/user')
 const adds = require('./mock-adds')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 
-const sequelize = new Sequelize( // Instancier Sequelize
-	'topannonces',
-	'root',
-	'root',
-	{
-		host: 'localhost',
-		dialect: 'mysql',
-		dialectOptions: {
-			socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+let sequelize
+if(process.env.NODE_ENV === 'production') {
+	sequelize = new Sequelize( // Instancier Sequelize
+		'qaixeeazf3rwdhr2',
+		'p290fnqids11b7fv',
+		'encwrrqkv6ch0te6',
+		{
+			host: 'ao9moanwus0rjiex.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+			dialect: 'mariadb',
+			dialectOptions: {
+				timezone: 'Etc/GMT-2'
+			},
+			loggin: true
 		}
-	}
-);
+	);
+} else {
+	sequelize = new Sequelize( // Instancier Sequelize
+		'topannonces',
+		'root',
+		'root',
+		{
+			host: 'localhost',
+			dialect: 'mysql',
+			dialectOptions: {
+				socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+			}
+		}
+	);
+}
 
 const Add = AddModel(sequelize, Sequelize.DataTypes) // Synchroniser la BDD avec le model
 const User = UserModel(sequelize, Sequelize.DataTypes)
 
 const initDb = () => {
-    return sequelize.sync({force:true}) .then(_ => {
+    return sequelize.sync() .then(_ => {
 		adds.map(add => {
 			Add.create({
 				title: add.title,
